929c3a377e82feabec7b6fb03ea647c260435f29
Theresececilia <therese.johansson@edu.cmeducations.se>
2022-11-22 11:51:31 +0100

    game done

---
+++ js/hangman.js
@@ -0,0 +1,99 @@
+let programming_languages = [
+    "python",
+    "javascript",
+    "java",
+    "html",
+    "css",
+    "csharp",
+    "kotlin",
+    "php"
+]
+
+let answer = "";
+let maxWrong = 6;
+let mistakes = 0;
+let guessed = [];
+let wordStatus = null;
+
+function randomWord() {
+    answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
+}
+
+function generateButtons() {
+    let buttonsHTML = "abcdefghijklmnopqrstuvxyz".split("").map(letter =>
+        `
+    <button
+      class="btn btn-lg btn-primary m-2"
+      id='` + letter + `'
+      onClick="handleGuess('` + letter + `')"
+    >
+    ` + letter + `
+    </button>
+    `).join("");
+
+    document.getElementById('keyboard').innerHTML = buttonsHTML;
+}
+
+function handleGuess(chosenLetter) {
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+    document.getElementById(chosenLetter).setAttribute("disabled", true);
+
+    if (answer.indexOf(chosenLetter) >= 0) {
+        guessedWord();
+        checkIfGameWon();
+    } else if (answer.indexOf(chosenLetter) === -1) {
+        mistakes++;
+        updateMistakes();
+        checkIfGameLost();
+        updateHangmanPicture();
+    }
+}
+
+function updateHangmanPicture() {
+    document.getElementById('hangmanPic').src = "../img/" + mistakes + ".jpg";
+}
+
+function guessedWord() {
+    wordStatus = answer.split("").map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join("");
+
+    document.getElementById("wordSpotlight").innerHTML = wordStatus;
+
+}
+
+function checkIfGameLost() {
+    if (mistakes === maxWrong) {  
+        document.getElementById("wordSpotlight").innerHTML = 'The answer was: ' + answer;
+        document.getElementById("keyboard").innerHTML = "Better Luck Next Time!"
+      
+    }
+}
+
+function checkIfGameWon() {
+    if (wordStatus === answer) {
+        document.getElementById("keyboard").innerHTML = "You Guessed Right!!!"
+    }
+}
+
+function updateMistakes() {
+    document.getElementById("mistakes").innerHTML = mistakes;
+}
+
+function reset() {
+    mistakes = 0;
+    guessed = [];
+    document.getElementById("hangmanPic").src = "../img/0.jpg";
+
+    randomWord();
+    guessedWord();
+    updateMistakes();
+    generateButtons();
+}
+
+document.getElementById("maxWrong").innerHTML = maxWrong;
+
+randomWord();
+generateButtons();
+guessedWord();
+handleGuess();
+
+
